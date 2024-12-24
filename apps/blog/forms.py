2320a8140ebb2f224afe9710537fa5e32aec32db
Aaron Elmquist <aaron.elmquist@protonmail.com>
2024-12-24 09:39:18 -0600

    cleans up css and layout on contact page

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -1,5 +1,5 @@
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
+from crispy_forms.layout import Submit, Layout, Field
 from django import forms
 from django.conf import settings
 from django_recaptcha.fields import ReCaptchaField
@@ -8,10 +8,9 @@ from django_recaptcha.widgets import ReCaptchaV2Checkbox
 class ContactForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(Submit("submit", "Send Email"))
+        self.fields["captcha"].label = False
 
-    sender = forms.CharField(max_length=256, label="From")
+    sender = forms.CharField(max_length=256)
     email = forms.EmailField(max_length=256)
     subject = forms.CharField(max_length=256)
     message = forms.CharField(
@@ -19,6 +18,16 @@ class ContactForm(forms.Form):
         widget=forms.Textarea(attrs=dict(cols=19, rows=10, placeholder="2000 character limit")),
     )
     captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
+
+    helper = FormHelper()
+    helper.add_input(Submit("submit", "Send Email"))
+    helper.layout = Layout(
+        Field("sender"),
+        Field("email"),
+        Field("subject"),
+        Field("message"),
+        Field('captcha', style="border: none; padding: 0px"),
+    )
 
     def send_email(self):
         self._send_email(
