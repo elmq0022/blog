5e0bda6494f445f04f77f9da0b4fcc625dbf27ee
Aaron Elmquist <aaron.elmquist@protonmail.com>
2024-12-30 10:18:04 -0600

    flatpages uses tinymce

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,7 +1,11 @@
-from tinymce.widgets import TinyMCE
 from django import forms
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
+from tinymce.widgets import TinyMCE
+
 from apps.blog.models import Article
+
 
 class ArticleForm(forms.ModelForm):
     class Meta:
@@ -9,8 +13,24 @@ class ArticleForm(forms.ModelForm):
         widgets = {"content": TinyMCE()}
         exclude = []
 
+
 class ArticleAdmin(admin.ModelAdmin):
     form = ArticleForm
 
 
 admin.site.register(Article, ArticleAdmin)
+
+
+class FlatPageForm(forms.ModelForm):
+    class Meta:
+        model = FlatPage
+        widgets = {'content': TinyMCE(attrs={'cols': 160, 'rows': 30})}
+        exclude = []
+
+
+class CustomFlatPateAdmin(FlatPageAdmin):
+    form = FlatPageForm
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, CustomFlatPateAdmin)
